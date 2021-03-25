6d454063197fa927c597ab728383ac259a5c6641
Chefkoch272 <thomas.stach1@gmx.de>
2021-03-25 16:09:50 +0100

    Add progress to script for problem file creation.

---
+++ create_prob_file.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+
+def main(problem_number):
+    f_name = 'prob' + str(problem_number).zfill(4) + '.py'
+    f = open(f_name, 'x')
+    # Todo: fill new file with template structure
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--problem_number', type=int)
+    args = parser.parse_args()
+    main(**vars(args))
