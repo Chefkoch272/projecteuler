bcd8372fe829c9ee96f3ef52178ac853c262607c
Chefkoch272 <thomas.stach1@gmx.de>
2021-03-25 14:32:00 +0100

    Add progress to scraper.

--- dev_scraper.py
+++ dev_scraper.py
@@ -1,11 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://projecteuler.net/problem=8'
+prob_n = 123
+
+URL_base = 'https://projecteuler.net/problem='
+URL = URL_base + str(prob_n)
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id='problem_info')
+prob_title = soup.find(id='problem_info').find('h3').text
+prob_info = soup.find(id='content').find('h2').text
+# Todo: Find solution to display special formats, e.g. superscript, correctly.
+# prob_description = soup.find(class_='problem_content').text
 
-print(results.prettify())
+print(prob_title)
+print(prob_info)
+# print(prob_description)
