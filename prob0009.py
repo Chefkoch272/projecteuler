6f2e984edac4792d0f23a4c243c49f712900f1cb
Chefkoch272 <thomas.stach1@gmx.de>
2021-08-27 18:45:23 +0200

    Add solution to prob 9.

--- prob0009.py
+++ prob0009.py
@@ -10,7 +10,20 @@ import argparse
 def main(number, description, verbose = False):
     if description:
         print(__doc__)
-    return number
+
+    solution = []
+
+    for c in range(number+1):
+        c_diff = number - c
+        for b in range(c_diff+1):
+            a = c_diff - b
+            if a < b and b < c:
+                if verbose:
+                    print(c, b, a)
+                if a**2 + b**2 == c**2:
+                    solution.append(a * b * c)
+
+    return solution
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
